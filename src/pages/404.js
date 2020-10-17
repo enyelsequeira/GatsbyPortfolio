f27a47adbea543e44431f26d8f0cd088b621617b
enyelsequeira <enyelsequeira@hotmail.com>
2020-10-17 16:35:29 +0100

    adding 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,8 @@ import PageLayout from "../layout/Layout"
 import SEO from "../utils/seo"
 import styled from "styled-components"
 
+import notFound from "../images/notFound.png"
+
 const BlockGroup = styled.div`
   display: flex;
 `
@@ -11,22 +13,37 @@ const BlockGroup = styled.div`
 const LeftBlock = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
 `
 
 const Title = styled.h1`
-  color: #002f34;
+  color: hsl(34.9,98.6%,72.9%);
+  margin: 2rem 0;
 `
 
 const Text = styled.p`
-  color: #9b92a0;
+  color:hsl(204,23.8%,95.9%);
+  font-size: 2rem;
+   margin-bottom: 2rem;
+
 `
 
 const ReturnBtn = styled(Link)`
   width: fit-content;
-  padding: 0.5rem 1rem;
-  background: #9b92a0;
+  padding: 0.5rem 2rem;
+  background: hsl(34.9,98.6%,72.9%);
   color: #002f34;
   border-radius: 5px;
+  margin: 2rem 0;
+`
+const NotFoundImage = styled.img`
+border: 1px solid;
+text-align: center;
+margin: 0 auto;
+border-radius: 40px;
+
 `
 
 const NotFoundPage = () => (
@@ -34,8 +51,9 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <BlockGroup>
       <LeftBlock>
-        <Title>Whooooops!</Title>
+        <Title>What you doing here??? </Title>
         <Text>Sorry, the page you are looking for doesn't exist</Text>
+        <NotFoundImage src={notFound} alt="NotFoundPage"/>
         <ReturnBtn to="/">Go Home</ReturnBtn>
       </LeftBlock>
     </BlockGroup>
